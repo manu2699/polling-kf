d69ff7f72171fd26a12db91c016900532a85b00c
Venkadesh S <venkadesh@kissflow.com>
2024-04-24 20:43:59 +0530

    ui fix

--- app/poll/page.jsx
+++ app/poll/page.jsx
@@ -18,9 +18,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col	gap-4">
-      <div className="h-10 px-6 flex gap-4 items-center justify-between w-full border-b-2">
-        <div>Polls Dashboard</div>
-        <Button variant="secondary">Create Poll</Button>
+      <div className="px-6 flex gap-4 items-center justify-between w-full border-b-2">
+        <div className="text-base	font-semibold	text-cyan-800	">
+          Polls Dashboard
+        </div>
+        <Button className="bg-cyan-900	text-white	m-2" variant="secondary">
+          Create Poll
+        </Button>
       </div>
 
       <div className="px-6">
@@ -42,7 +46,9 @@ function PollsList({ pollsList, myPoll = false }) {
           className="h-10 p-5 flex items-center justify-between border-b-2	"
           key={pollInfo.id}
         >
-          <span>{pollInfo.question}</span>
+          <span className="text-sm font-medium text-slate-600		">
+            {pollInfo.question}
+          </span>
           {myPoll && <Button variant="secondary">View report</Button>}
         </div>
       ))}
@@ -51,5 +57,9 @@ function PollsList({ pollsList, myPoll = false }) {
 }
 
 function Title({ children }) {
-  return <div className="mb-4">{children}</div>;
+  return (
+    <div className="mb-4 text-base	font-medium border-cyan-800text-slate-800		">
+      {children}
+    </div>
+  );
 }
