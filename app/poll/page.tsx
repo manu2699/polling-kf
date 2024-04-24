bd7635fc3d16f31c181aa4276224e5d731b1d44d
KarthickSakthi <karthicksakthi124@gmail.com>
2024-04-24 16:20:43 +0530

    nextjs report poll setup

--- app/poll/page.tsx
+++ app/poll/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div className={""}>Poll route </div>;
+  return (
+    
+  )
 }
---
+++ app/poll/report/report.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export function PollReport(){
+    return(
+        <h1>Report Page</h1>    )
+}
